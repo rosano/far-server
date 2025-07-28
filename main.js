10d1a48b57e770e5b0241218e0f0c513170f2ed6
Rosano Coutinho <github.com@xeari.com>
2025-07-28 13:27:54 +0100

    parse body

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 require('dotenv').config({ quiet: true })
 
 app.use(async (req, res) => {
